5fc89eda4b561531df064b9df4aa5a5943ace9d5
0331Gavin <15834198567@139.com>
2018-11-26 14:43:43 +0800

    init2

--- src/main/java/com/eastrise/BjtpbApplication.java
+++ src/main/java/com/eastrise/BjtpbApplication.java
@@ -1,6 +1,5 @@
 package com.eastrise;
 
-import com.baidu.ueditor.ActionEnter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
